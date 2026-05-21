881da9468b093a371710321eeaddb743013d8e48
Giang <anhrutro@gmail.com>
2026-05-21 15:03:42 +0700

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import path from "node:path"
 
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import { cloudflare } from "@cloudflare/vite-plugin"
 
 export default defineConfig({
   plugins: [
     react(),
-    cloudflare(),
   ],
   resolve: {
     alias: {
